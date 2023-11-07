ffc0666450da4bff3b6a66805aaace6891e77b0c
Xander-Crocker <Xander-Crocker@live.nmit.ac.nz>
2023-11-08 09:58:24 +1300

    rework

--- routes/order/index.js
+++ routes/order/index.js
@@ -5,14 +5,14 @@ var router = express.Router();
 //SETUP - Import Routes
 const all = require('./all')
 const create = require('./create')
-const update = require('./update')
-const cancel = require('./delete')
+//const update = require('./update')
+//const cancel = require('../product/delete')
 
 
 router.use('/', all);
 router.use('/', create);
-router.use('/', update);
-router.use('/', Delete);
+//router.use('/', update);
+//router.use('/', Delete);
 
 
 module.exports = router;
